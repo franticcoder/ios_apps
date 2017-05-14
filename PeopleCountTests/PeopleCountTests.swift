c78e3e36a9eb95f1cda305afc849b642df98f4f2
KCM <kim.chungmin@gmail.com>
2017-05-14 19:53:12 -0400

    Initial Commit

---
+++ PeopleCountTests/PeopleCountTests.swift
@@ -0,0 +1,36 @@
+//
+//  PeopleCountTests.swift
+//  PeopleCountTests
+//
+//  Created by kcmmac on 2017-05-14.
+//  Copyright © 2017 kcmmac. All rights reserved.
+//
+
+import XCTest
+@testable import PeopleCount
+
+class PeopleCountTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
